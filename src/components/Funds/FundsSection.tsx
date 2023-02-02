e79c424bc4f8ce7ef3ba620d46db3a74862bace1
Marcus Pang <marcuspangyuyang@gmail.com>
2023-02-02 20:10:07 +0800

    feat: fix mobile dialog box

--- src/components/Funds/FundsSection.tsx
+++ src/components/Funds/FundsSection.tsx
@@ -1,15 +1,39 @@
+import { useState } from 'react';
 import NormalButton from '../Layout/NormalButton';
 import PageTitle from '../Layout/PageTitle';
 import Fund from './Fund';
 
+enum ViewState {
+  ALL,
+  MANAGED_BY_USER,
+}
+
 const FundsSection = () => {
+  const [viewState, setViewState] = useState(ViewState.ALL);
+
   return (
     <>
       <PageTitle title="Funds" />
       <div className="flex justify-end space-x-4 items-center pb-6">
-        <p className="text-white text-lg">View: </p>
-        <NormalButton title="All" />
-        <NormalButton title="Managed" />
+        <p className="text-white text-lg">Filter Funds: </p>
+        <NormalButton
+          title="All"
+          onClick={() => setViewState(ViewState.ALL)}
+          className={
+            viewState === ViewState.ALL
+              ? 'bg-purple-700 text-purple-100 hover:bg-purple-600 underline'
+              : 'bg-purple-600 hover:bg-purple-700'
+          }
+        />
+        <NormalButton
+          title="Managed by you"
+          onClick={() => setViewState(ViewState.MANAGED_BY_USER)}
+          className={
+            viewState === ViewState.MANAGED_BY_USER
+              ? 'bg-purple-700 text-purple-100 hover:bg-purple-600 underline'
+              : 'bg-purple-600 hover:bg-purple-700'
+          }
+        />
       </div>
       <div className="flex space-x-2">
         <Fund
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -14,15 +14,15 @@ export default function Header() {
   return (
     <Popover className="relative bg-purple-900" as={'header'}>
       <div className="mx-auto px-6">
-        <div className="flex items-center justify-between border-b-2 border-purple-900 py-6 md:justify-start md:space-x-10">
+        <div className="flex items-center justify-between border-b-2 border-purple-900 py-6 lg:justify-start lg:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <Link href="/">
               <span className="sr-only">Fund Asset Manager</span>
               <p className="text-purple-200 font-bold">Alfred Protocol</p>
             </Link>
           </div>
-          <div className="-my-2 -mr-2 md:hidden">
-            <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+          <div className="-my-2 -mr-2 lg:hidden">
+            <Popover.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-purple-800 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 transition-all">
               <span className="sr-only">Open menu</span>
               <Bars3Icon
                 className="h-6 w-6 stroke-2 stroke-purple-200"
@@ -30,7 +30,7 @@ export default function Header() {
               />
             </Popover.Button>
           </div>
-          <Popover.Group as="nav" className="hidden space-x-10 md:flex">
+          <Popover.Group as="nav" className="hidden space-x-10 lg:flex px-6">
             {links.map(({ name, href }) => (
               <Link
                 key={name}
@@ -41,7 +41,7 @@ export default function Header() {
               </Link>
             ))}
           </Popover.Group>
-          <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+          <div className="hidden items-center justify-end lg:flex lg:flex-1 lg:w-0">
             <ConnectButton />
           </div>
         </div>
@@ -58,55 +58,35 @@ export default function Header() {
       >
         <Popover.Panel
           focus
-          className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden"
+          className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition lg:hidden"
         >
-          <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
-            <div className="px-5 pt-5 pb-6">
+          <div className="divide-y-2 divide-purple-900 rounded-lg bg-purple-700 shadow-lg ring-1 ring-black ring-opacity-5">
+            <div className="px-5 pt-5 pb-2">
               <div className="flex items-center justify-between">
                 <div>
-                  <p>Logo</p>
+                  <p className="text-white">Logo</p>
                 </div>
                 <div className="-mr-2">
-                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-purple-700 p-2 text-purple-300  hover:text-purple-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 transition-all">
                     <span className="sr-only">Close menu</span>
                     <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
                 </div>
               </div>
-              <div className="mt-6">
-                <nav className="grid gap-y-8"></nav>
-              </div>
             </div>
             <div className="space-y-6 py-6 px-5">
-              <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                <a
-                  href="#"
-                  className="text-base font-medium text-gray-900 hover:text-gray-700"
-                >
-                  Pricing
-                </a>
-
-                <a
-                  href="#"
-                  className="text-base font-medium text-gray-900 hover:text-gray-700"
-                >
-                  Docs
-                </a>
+              <div className="grid gap-y-4">
+                {links.map(({ name, href }) => (
+                  <Link
+                    key={name}
+                    href={href}
+                    className="text-base font-medium text-purple-100 hover:text-purple-300 transition-all"
+                  >
+                    {name}
+                  </Link>
+                ))}
               </div>
-              <div>
-                <a
-                  href="#"
-                  className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-                >
-                  Sign up
-                </a>
-                <p className="mt-6 text-center text-base font-medium text-gray-500">
-                  Existing customer?{' '}
-                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
-                    Sign in
-                  </a>
-                </p>
-              </div>
+              <ConnectButton />
             </div>
           </div>
         </Popover.Panel>

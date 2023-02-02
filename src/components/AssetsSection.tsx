1499a237ed43c007839dabb7b8d76cb2f69ee2ce
kd in <kevin.tirta@okg.com>
2023-02-02 17:38:02 +0800

    [add] modify asset card

--- src/components/AssetsSection.tsx
+++ src/components/AssetsSection.tsx
@@ -3,6 +3,62 @@ import { useState } from 'react';
 import Card from './Card';
 import PageTitle from './Layout/PageTitle';
 
+export type Fund = {
+  fundName: String;
+  tvl: number;
+  manager: String;
+  assets: { assetName: String; assetValue: number }[];
+  depositEnable: boolean;
+  withdrawEnable: boolean;
+};
+
+const mockData: [Fund] = [
+  {
+    fundName: 'Fund A',
+    tvl: 234,
+    manager: 'ox23CjdS932923SAJ',
+    assets: [
+      { assetName: 'ETH', assetValue: 1212 },
+      { assetName: 'USDC', assetValue: 1212 },
+    ],
+    depositEnable: true,
+    withdrawEnable: true,
+  },
+  {
+    fundName: 'Fund B',
+    tvl: 234,
+    manager: 'ox23CjdS932923SAJ',
+    assets: [
+      { assetName: 'ETH', assetValue: 1212 },
+      { assetName: 'USDC', assetValue: 1212 },
+    ],
+    depositEnable: true,
+    withdrawEnable: true,
+  },
+  {
+    fundName: 'Fund C',
+    tvl: 234,
+    manager: 'ox23CjdS932923SAJ',
+    assets: [
+      { assetName: 'ETH', assetValue: 1212 },
+      { assetName: 'USDC', assetValue: 1212 },
+    ],
+    depositEnable: true,
+    withdrawEnable: true,
+  },
+  {
+    fundName: 'Fund D',
+    tvl: 234,
+    manager: 'ox23CjdS932923SAJ',
+    assets: [
+      { assetName: 'ETH', assetValue: 1212 },
+      { assetName: 'USDC', assetValue: 1212 },
+    ],
+    depositEnable: true,
+    withdrawEnable: true,
+  },
+];
+
 const AssetsSection = () => {
   const [assets, setAssets] = useState([]);
 
@@ -11,11 +67,9 @@ const AssetsSection = () => {
     <>
       <PageTitle title="Assets" />
       <div className="flex flex-row flex-wrap">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {mockData.map((fund) => {
+          return <Card {...fund} />;
+        })}
       </div>
     </>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,24 +1,35 @@
 import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline';
 
-const mockData = [
-  {
-    fundName: 'Fund A',
-    tvl: 234,
-  },
-];
+import { Fund } from './AssetsSection';
 
-const AssetCard = () => {
+interface Props {
+  fundName: String;
+  tvl: number;
+  manager: String;
+  assets: { assetName: String; assetValue: number }[];
+  depositEnable: boolean;
+  withdrawEnable: boolean;
+}
+
+const AssetCard = ({
+  fundName,
+  tvl,
+  manager,
+  assets,
+  depositEnable,
+  withdrawEnable,
+}: Props) => {
   return (
-    <div className="bg-white py-4 px-4 rounded shadow text-left ml-10 mr-10 mb-10">
-      <h3 className="font-bold text-purple-900 text-xl">DAI / USDC</h3>
+    <div className="bg-white w-200 py-4 px-4 rounded shadow text-left ml-10 mr-10 mb-10 w-1/4">
+      <h3 className="font-bold text-purple-900 text-xl">{fundName}</h3>
       <p>
-        <span className="font-semibold">Total Liquidity: </span>
-        <span>10 ETH</span>
+        <span className="font-semibold">TVL: </span>
+        <span>{tvl} USDC</span>
       </p>
       <p>
         <span className="font-semibold">Manager: </span>
         <span>
-          0xf23c75Bc0e48Ac25883392D63DA556cB8aF40BA3
+          {manager}
           <ArrowTopRightOnSquareIcon
             height={20}
             width={20}
@@ -27,7 +38,32 @@ const AssetCard = () => {
           />
         </span>
       </p>
+      <p>
+        <span className="font-semibold">Your Asset: </span>
+      </p>
+      {assets.map((asset) => {
+        return (
+          <p>
+            <span className="font-semibold">{asset.assetName}: </span>
+            <span>{asset.assetValue}</span>
+          </p>
+        );
+      })}
+
+      <div className="mt-2">
+        {depositEnable && (
+          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mr-3">
+            Deposit
+          </button>
+        )}
+        {withdrawEnable && (
+          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Withdraw
+          </button>
+        )}
+      </div>
     </div>
   );
+  å;
 };
 export default AssetCard;

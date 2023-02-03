7cbfdda12bfa3f15d299bfb6145d1460f1bcf6fb
kd in <kevin.tirta@okg.com>
2023-02-03 11:29:02 +0800

    ! fix

--- src/pages/funds/index.tsx
+++ src/pages/funds/index.tsx
@@ -1,8 +1,19 @@
 import FundsSection from '@/components/Funds/FundsSection';
 import Layout from '@/components/Layout/Layout';
 import PageTitle from '@/components/Layout/PageTitle';
+import { useEffect, useState } from 'react';
 
 export default function FundsPage() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  if (!loaded) {
+    return;
+  }
+
   return (
     <Layout>
       <PageTitle title="Funds" />

0d66810458365b92458fbbe48cf59c5f1ec06f66
kd in <kevin.tirta@okg.com>
2023-02-05 15:45:47 +0800

    add particle animation

--- src/components/Homepage/HeroSection.tsx
+++ src/components/Homepage/HeroSection.tsx
@@ -1,13 +1,39 @@
+import { useCallback } from 'react';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { TypeAnimation } from 'react-type-animation';
 import FancyButton from '../Layout/FancyButton';
 import PageTitle from '../Layout/PageTitle';
+import Particles from 'react-tsparticles';
+import type { Container, Engine } from 'tsparticles-engine';
+import { loadFull } from 'tsparticles';
+import animationJSON from 'src/assets/particles.json';
 
 // TODO: add wavy background? https://kevinhufnagl.com/how-to-stripe-website-gradient-effect/
 const HeroSection = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
+
+    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    []
+  );
   return (
     <div>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={animationJSON}
+      />
       <PageTitle title="Decentralised Fund Manager" />
       <TypeAnimation
         sequence={[
@@ -21,7 +47,7 @@ const HeroSection = () => {
         cursor={true}
         repeat={Infinity}
         style={{ fontSize: '2em' }}
-        className="text-white text-xl md:text-3xl"
+        className="text-xl text-white md:text-3xl"
       />
       <FancyButton className="mt-8">
         <Link href="/funds">

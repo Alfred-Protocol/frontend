65c9773ae6934d555f16fb7b80ebbd1cef67dbbc
kevin.tirta <kevin.tirta@okg.com>
2023-02-08 00:49:44 +0800

    ! add animation

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -6,16 +6,21 @@ import SelectPairModal from '@/components/select-pair/SelectPairModal';
 import { useAppContext } from '@/context/app/appContext';
 import ContextProvider from '@/context/ContextProvider';
 import { useEffect, useState } from 'react';
-import { fadeIn } from 'react-animations';
+import { fadeIn, bounce } from 'react-animations';
 import Radium, { StyleRoot } from 'radium';
 
 const styles = {
-  bounce: {
+  fadeIn: {
     animation: 'x 1s',
     animationName: Radium.keyframes(fadeIn, 'fadeIn'),
   },
+  bounce: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(bounce, 'bounce'),
+  },
 };
 import CreatePosition from '../../components/FundDetails/CreatePosition';
+import CustomButton from '@/components/Common/CustomButton';
 
 const AboutPage = () => {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -34,9 +39,19 @@ const AboutPage = () => {
       <Layout>
         <div className="flex w-full justify-center">
           <div className="mt-10 flex w-4/5 flex-col space-y-10">
-            {showPairModal && (
-              <div style={styles.bounce}>
+            {showPairModal ? (
+              <div style={styles.fadeIn}>
                 <SelectPairModal submitEnded={() => setShowPairModal(false)} />
+              </div>
+            ) : (
+              <div className="flex justify-center">
+                <CustomButton
+                  title="Change Settings"
+                  theme="solidPurple"
+                  className="w-1/5"
+                  onClick={() => setShowPairModal(true)}
+                  style={styles.bounce}
+                />
               </div>
             )}
             <div className="flex space-x-5">
@@ -46,7 +61,7 @@ const AboutPage = () => {
             <LiquidityPositionChart />
           </div>
         </div>
-        <CreatePosition />
+        {/* <CreatePosition /> */}
       </Layout>
     </StyleRoot>
   );

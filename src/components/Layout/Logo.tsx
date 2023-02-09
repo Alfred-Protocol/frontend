ae8a4bad7c5f4a2bd8147c813b9b08560ce9eb93
Marcus Pang <marcuspangyuyang@gmail.com>
2023-02-09 18:34:27 +0800

    feat: move assets to public folder

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -7,7 +7,9 @@ const Logo = () => {
       <AlfredLogo width={20} />
       <Link href="/">
         <span className="sr-only">Alfred Protocol</span>
-        <p className="font-bold sm:text-fuchsia-50 text-purple-200">Alfred Protocol</p>
+        <p className="font-bold text-purple-200 transition-colors hover:text-fuchsia-200 sm:text-fuchsia-50">
+          Alfred Protocol
+        </p>
       </Link>
     </div>
   );

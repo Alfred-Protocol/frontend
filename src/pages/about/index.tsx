7f88a95a5c93a2401e7f9b8c3ea02032987df135
Marcus Pang <marcuspangyuyang@gmail.com>
2023-02-10 01:35:36 +0800

    feat: add roadmap

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,8 +1,8 @@
 import Layout from '@/components/Layout/Layout';
-import { useAppContext } from '@/context/app/appContext';
-import Radium from 'radium';
+import PageTitle from '@/components/Layout/PageTitle';
+import { CalendarDaysIcon, SunIcon } from '@heroicons/react/24/outline';
+import { Timeline } from 'flowbite-react';
 import { useEffect, useState } from 'react';
-import { bounce, fadeIn } from 'react-animations';
 
 const AboutPage = () => {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -16,13 +16,59 @@ const AboutPage = () => {
   }
 
   return (
-    <Radium.StyleRoot>
-      <Layout>
-        <div className="flex w-full justify-center">
-          <div className="mt-10 flex w-4/5 flex-col space-y-10"></div>
-        </div>
-      </Layout>
-    </Radium.StyleRoot>
+    <Layout>
+      <PageTitle title="Roadmap" />
+      <div className="mx-auto max-w-6xl rounded-xl border-2 border-[#EF5DA8] bg-blackfill py-4 px-8 pb-6 text-left">
+        <Timeline horizontal className="space-x-6 pt-8">
+          <Timeline.Item>
+            <Timeline.Point />
+            <Timeline.Content>
+              <Timeline.Title>Genesis - Onboarding competition</Timeline.Title>
+              <Timeline.Time>Q2 2023</Timeline.Time>
+              <Timeline.Body></Timeline.Body>
+            </Timeline.Content>
+          </Timeline.Item>
+          <Timeline.Item>
+            <Timeline.Point />
+            <Timeline.Content>
+              <Timeline.Title>Mainnet - Investor onboarding</Timeline.Title>
+              <Timeline.Time>Q4 2023</Timeline.Time>
+              <Timeline.Body></Timeline.Body>
+            </Timeline.Content>
+          </Timeline.Item>
+          <Timeline.Item>
+            <Timeline.Point />
+            <Timeline.Content>
+              <Timeline.Title>
+                Wormhole bridge - LP on additional chains
+              </Timeline.Title>
+              <Timeline.Time>Q2 2024</Timeline.Time>
+              <Timeline.Body></Timeline.Body>
+            </Timeline.Content>
+          </Timeline.Item>
+          <Timeline.Item>
+            <Timeline.Point />
+            <Timeline.Content>
+              <Timeline.Title>
+                Voting on fee tier value and structure
+              </Timeline.Title>
+              <Timeline.Time>Q3 2024</Timeline.Time>
+              <Timeline.Body></Timeline.Body>
+            </Timeline.Content>
+          </Timeline.Item>
+          <Timeline.Item>
+            <Timeline.Point />
+            <Timeline.Content>
+              <Timeline.Title>
+                Additional butler strategies - e.g., delta neutral IL
+              </Timeline.Title>
+              <Timeline.Time>Q3 2024</Timeline.Time>
+              <Timeline.Body></Timeline.Body>
+            </Timeline.Content>
+          </Timeline.Item>
+        </Timeline>
+      </div>
+    </Layout>
   );
 };
 export default AboutPage;

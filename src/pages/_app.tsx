ac54abf9840c94dad2024deae512d1db90833250
Marcus Pang <marcuspangyuyang@gmail.com>
2023-02-09 15:24:56 +0800

    feat: hardcode dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -75,14 +75,8 @@ const wagmiClient = createClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    if (
-      localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
-    ) {
+    if (document) {
       document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
     }
   }, []);
   return (
@@ -101,6 +95,7 @@ export default function App({ Component, pageProps }: AppProps) {
             closeOnClick={false}
             position="bottom-center"
             theme="dark"
+            hideProgressBar
           />
         </ContextProvider>
       </RainbowKitProvider>

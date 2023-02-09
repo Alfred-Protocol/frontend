6814e2af4e6f6514a85c06f26bd30ec308eb15f5
kevin.tirta <kevin.tirta@okg.com>
2023-02-09 22:10:59 +0800

    fix table styling

--- src/components/ManageFunds/ManageFundCard.tsx
+++ src/components/ManageFunds/ManageFundCard.tsx
@@ -82,80 +82,74 @@ const ManageFundCard = ({
 
   return (
     <div
-      className="w-full cursor-pointer rounded-xl border-2 border-[#EF5DA8] bg-blackfill py-6 px-8 text-left text-white transition-all hover:bg-gray-800"
+      className="flex w-full cursor-pointer rounded-xl border-2 border-[#EF5DA8] bg-blackfill py-6 px-8 text-left text-white transition-all hover:bg-gray-800"
       onClick={() => router.push(`/funds/${address}`)}
     >
-      <div className="relative mb-4 flex items-center">
+      <div className="relative mb-4 flex flex-col">
         <h3 className="text-2xl font-bold text-fuchsia-100 sm:text-4xl">
           {name}
         </h3>
-        <div className="ml-36 flex justify-center space-x-6">
-          <div className="flex items-center space-x-2">
-            <Image
-              src={logo1}
-              width={40}
-              height={40}
-              alt={'WETH'}
-              className="rounded-full"
-              style={{ borderRadius: 100 }}
-            />
-            <p className="text-2xl">{amount0.toLocaleString()}</p>
+
+        <PairValue field="TVL" value={totalValueLocked + ' ETH'} />
+        <PairValue
+          field="Yield"
+          value={`${yieldPercentage}%`}
+          valueClassName="text-green-500"
+          endComponent={
+            <Tooltip
+              content="Lifetime yield earned"
+              className="px-2 text-center"
+            >
+              <InformationCircleIcon
+                height={16}
+                width={16}
+                className="ml-1 transition-colors hover:stroke-fuchsia-300"
+              />
+            </Tooltip>
+          }
+        />
+        <PairValue
+          field="Start Date"
+          value={new Date(
+            startDate ? startDate.toString() : startDate
+          ).toLocaleDateString()}
+        />
+        <PairValue
+          field="Mature Date"
+          value={new Date(
+            matureDate ? matureDate.toString() : matureDate
+          ).toLocaleDateString()}
+        />
+      </div>
+      <div className="ml-4 flex flex-col">
+        <div className="flex justify-between">
+          <div className="flex space-x-6">
+            <div className="flex items-center space-x-2">
+              <Image
+                src={logo1}
+                width={40}
+                height={40}
+                alt={'WETH'}
+                className="rounded-full"
+                style={{ borderRadius: 100 }}
+              />
+              <p className="text-2xl">{amount0.toLocaleString()}</p>
+            </div>
+            <div className="flex items-center justify-center space-x-2">
+              <Image
+                src={logo2}
+                width={40}
+                height={40}
+                alt={'USDC'}
+                className="rounded-full"
+                style={{ borderRadius: 100 }}
+              />
+              <p className="text-2xl">{amount1.toLocaleString()}</p>
+            </div>
           </div>
-          <div className="flex items-center justify-center space-x-2">
-            <Image
-              src={logo2}
-              width={40}
-              height={40}
-              alt={'USDC'}
-              className="rounded-full"
-              style={{ borderRadius: 100 }}
-            />
-            <p className="text-2xl">{amount1.toLocaleString()}</p>
-          </div>
-        </div>
-        <div className="absolute right-0">
           <CustomButton title="Add Position" theme="solidPurple" className="" />
         </div>
-      </div>
-      <div className="flex">
-        <div className="mr-12">
-          <PairValue field="TVL" value={totalValueLocked + ' ETH'} />
-          <PairValue
-            field="Yield"
-            value={`${yieldPercentage}%`}
-            valueClassName="text-green-500"
-            endComponent={
-              <Tooltip
-                content="Lifetime yield earned"
-                className="px-2 text-center"
-              >
-                <InformationCircleIcon
-                  height={16}
-                  width={16}
-                  className="ml-1 transition-colors hover:stroke-fuchsia-300"
-                />
-              </Tooltip>
-            }
-          />
-          <PairValue
-            field="Start Date"
-            value={new Date(
-              startDate ? startDate.toString() : startDate
-            ).toLocaleDateString()}
-          />
-          <PairValue
-            field="Mature Date"
-            value={new Date(
-              matureDate ? matureDate.toString() : matureDate
-            ).toLocaleDateString()}
-          />
-        </div>
-        <div className="flex flex-col">
-          <div className="flex flex-1 space-x-6"></div>
-          <div className="flex-1">
-            <FundTableList />
-          </div>
-        </div>
+        <FundTableList />
       </div>
     </div>
   );

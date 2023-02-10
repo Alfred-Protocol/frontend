eec061326a5e91e8d135e9c827908866f4f7ffe4
Marcus Pang <marcuspangyuyang@gmail.com>
2023-02-10 13:02:26 +0800

    feat: improve responsiveness

--- src/components/Assets/AssetsHeader.tsx
+++ src/components/Assets/AssetsHeader.tsx
@@ -10,17 +10,17 @@ interface Props {
 const AssetsHeader = ({ managerAddress, netDeposit, netValue }: Props) => {
   return (
     <div className="mb-12 mt-10 w-full rounded-xl border-2 border-[#EF5DA8] bg-blackfill py-8 px-8 text-left text-fuchsia-100 ">
-      <div className="mb-10 text-3xl font-bold md:text-5xl">
+      <div className="mb-10 text-4xl font-bold md:text-5xl">
         <span className="pr-1">👋</span>
         <span>Welcome Back!</span>
       </div>
-      <div className="flex">
+      <div className="flex flex-col md:flex-row">
         <div className="flex-1">
-          <div className="relative mb-2 flex items-center lg:flex-wrap">
-            <p className="text-lg font-bold md:text-2xl">Net WMATIC Value</p>
+          <div className="mb-2 flex items-center">
+            <p className="text-2xl font-bold">Net WMATIC Value</p>
             <Tooltip
               content="Total net value of your funds"
-              className="text-center shadow-xl px-2"
+              className="px-2 text-center shadow-xl"
             >
               <InformationCircleIcon
                 height={20}
@@ -29,14 +29,14 @@ const AssetsHeader = ({ managerAddress, netDeposit, netValue }: Props) => {
               />
             </Tooltip>
           </div>
-          <p className="mb-2 text-4xl font-thin">{netValue.toFixed(3)}</p>
+          <p className="mb-2 text-4xl font-thin">{netValue.toFixed(5)}</p>
         </div>
         <div className="flex-1">
-          <div className="relative mb-2 flex items-center lg:flex-wrap">
-            <p className="md;text-2xl text-lg font-bold">Net WMATIC Deposits</p>
+          <div className="mb-2 flex items-center">
+            <p className="text-2xl font-bold">Net WMATIC Deposits</p>
             <Tooltip
-              content="Total net value of your funds"
-              className="text-center shadow-xl px-2"
+              content="Total value of your deposits in all funds"
+              className="px-2 text-center shadow-xl"
             >
               <InformationCircleIcon
                 height={20}
@@ -45,7 +45,7 @@ const AssetsHeader = ({ managerAddress, netDeposit, netValue }: Props) => {
               />
             </Tooltip>
           </div>{' '}
-          <p className="mb-2 text-4xl font-thin">{netDeposit.toFixed(3)}</p>
+          <p className="mb-2 text-4xl font-thin">{netDeposit.toFixed(5)}</p>
         </div>
       </div>
     </div>
